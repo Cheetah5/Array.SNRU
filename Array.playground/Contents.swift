02011fd0c8ec9903ef2810da0a1c563b9a255b9c
Student03 <phrombutr@gmail.comm>
2017-06-13 13:25:10 +0700

    StartApp

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// การใช้ตัวแปร Array
+// การประกาศตัวแปร Array และกำหนดค่า
+//Implicit
+let intArrayNumber1 = [11,22,33,44]
+
+//Explicit
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [55,66,77,88]
+
+//String Type
+var strArrayName1 = ["Doramon", "Nobita","นิว"]
+var strArrayName2 = [String]()
+strArrayName2 = ["AAA", "BBB", "CCC"]
+
+//Double
+var douArrayNum3 = [1.2,1.3,1.4]
+var douArrayNum4 = [Double]()
+douArrayNum4 = [2.1,2.2,2.3]
+
+
+
