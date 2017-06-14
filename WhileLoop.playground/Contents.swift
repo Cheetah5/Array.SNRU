8dcd2838829da5d998ff31f08472f6693609d97e
Student03 <phrombutr@gmail.comm>
2017-06-14 14:08:00 +0700

    While Loop

---
+++ WhileLoop.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//While Loop Basic
+var intTimes = 1
+
+while intTimes <= 10 {
+  print("intTimes ==> \(intTimes)")
+    intTimes += 1
+   }
+
+// สูตรคูณแม่ 2
+var intTimes2 = 1
+while intTimes2 <= 12 {
+    
+        print("2 x \(intTimes2)= \(intTimes2 * 2)")
+    intTimes2 += 1
+}
+
+let strArrayBook = ["Math","Physic","Cheme","Bio","Computer","English" ]
+var intIndexBook = 0
+
+while intIndexBook < strArrayBook.count {
+    print("This Book \(strArrayBook[intIndexBook])" )
+    intIndexBook += 1
+}
+
+
+
+
+
